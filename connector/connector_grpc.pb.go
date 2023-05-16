da2be7c05f60e12900259653d4385cbbfbabd04a
Bas Toonk <bas@toonk.nl>
2023-05-16 12:49:20 +0200

    update protobuf

--- connector/connector_grpc.pb.go
+++ connector/connector_grpc.pb.go
@@ -22,8 +22,6 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type ConnectorServiceClient interface {
-	Register(ctx context.Context, in *RegisterRequest, opts ...grpc.CallOption) (*RegisterResponse, error)
-	WatchMessages(ctx context.Context, in *WatchMessagesRequest, opts ...grpc.CallOption) (ConnectorService_WatchMessagesClient, error)
 	ControlStream(ctx context.Context, opts ...grpc.CallOption) (ConnectorService_ControlStreamClient, error)
 }
 
@@ -35,49 +33,8 @@ func NewConnectorServiceClient(cc grpc.ClientConnInterface) ConnectorServiceClie
 	return &connectorServiceClient{cc}
 }
 
-func (c *connectorServiceClient) Register(ctx context.Context, in *RegisterRequest, opts ...grpc.CallOption) (*RegisterResponse, error) {
-	out := new(RegisterResponse)
-	err := c.cc.Invoke(ctx, "/border0.v1.ConnectorService/Register", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *connectorServiceClient) WatchMessages(ctx context.Context, in *WatchMessagesRequest, opts ...grpc.CallOption) (ConnectorService_WatchMessagesClient, error) {
-	stream, err := c.cc.NewStream(ctx, &ConnectorService_ServiceDesc.Streams[0], "/border0.v1.ConnectorService/WatchMessages", opts...)
-	if err != nil {
-		return nil, err
-	}
-	x := &connectorServiceWatchMessagesClient{stream}
-	if err := x.ClientStream.SendMsg(in); err != nil {
-		return nil, err
-	}
-	if err := x.ClientStream.CloseSend(); err != nil {
-		return nil, err
-	}
-	return x, nil
-}
-
-type ConnectorService_WatchMessagesClient interface {
-	Recv() (*Message, error)
-	grpc.ClientStream
-}
-
-type connectorServiceWatchMessagesClient struct {
-	grpc.ClientStream
-}
-
-func (x *connectorServiceWatchMessagesClient) Recv() (*Message, error) {
-	m := new(Message)
-	if err := x.ClientStream.RecvMsg(m); err != nil {
-		return nil, err
-	}
-	return m, nil
-}
-
 func (c *connectorServiceClient) ControlStream(ctx context.Context, opts ...grpc.CallOption) (ConnectorService_ControlStreamClient, error) {
-	stream, err := c.cc.NewStream(ctx, &ConnectorService_ServiceDesc.Streams[1], "/border0.v1.ConnectorService/ControlStream", opts...)
+	stream, err := c.cc.NewStream(ctx, &ConnectorService_ServiceDesc.Streams[0], "/border0.v1.ConnectorService/ControlStream", opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -111,8 +68,6 @@ func (x *connectorServiceControlStreamClient) Recv() (*ControlStreamReponse, err
 // All implementations must embed UnimplementedConnectorServiceServer
 // for forward compatibility
 type ConnectorServiceServer interface {
-	Register(context.Context, *RegisterRequest) (*RegisterResponse, error)
-	WatchMessages(*WatchMessagesRequest, ConnectorService_WatchMessagesServer) error
 	ControlStream(ConnectorService_ControlStreamServer) error
 	mustEmbedUnimplementedConnectorServiceServer()
 }
@@ -121,12 +76,6 @@ type ConnectorServiceServer interface {
 type UnimplementedConnectorServiceServer struct {
 }
 
-func (UnimplementedConnectorServiceServer) Register(context.Context, *RegisterRequest) (*RegisterResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Register not implemented")
-}
-func (UnimplementedConnectorServiceServer) WatchMessages(*WatchMessagesRequest, ConnectorService_WatchMessagesServer) error {
-	return status.Errorf(codes.Unimplemented, "method WatchMessages not implemented")
-}
 func (UnimplementedConnectorServiceServer) ControlStream(ConnectorService_ControlStreamServer) error {
 	return status.Errorf(codes.Unimplemented, "method ControlStream not implemented")
 }
@@ -141,45 +90,6 @@ type UnsafeConnectorServiceServer interface {
 
 func RegisterConnectorServiceServer(s grpc.ServiceRegistrar, srv ConnectorServiceServer) {
 	s.RegisterService(&ConnectorService_ServiceDesc, srv)
-}
-
-func _ConnectorService_Register_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(RegisterRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(ConnectorServiceServer).Register(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/border0.v1.ConnectorService/Register",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ConnectorServiceServer).Register(ctx, req.(*RegisterRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _ConnectorService_WatchMessages_Handler(srv interface{}, stream grpc.ServerStream) error {
-	m := new(WatchMessagesRequest)
-	if err := stream.RecvMsg(m); err != nil {
-		return err
-	}
-	return srv.(ConnectorServiceServer).WatchMessages(m, &connectorServiceWatchMessagesServer{stream})
-}
-
-type ConnectorService_WatchMessagesServer interface {
-	Send(*Message) error
-	grpc.ServerStream
-}
-
-type connectorServiceWatchMessagesServer struct {
-	grpc.ServerStream
-}
-
-func (x *connectorServiceWatchMessagesServer) Send(m *Message) error {
-	return x.ServerStream.SendMsg(m)
 }
 
 func _ConnectorService_ControlStream_Handler(srv interface{}, stream grpc.ServerStream) error {
@@ -214,18 +124,8 @@ func (x *connectorServiceControlStreamServer) Recv() (*ControlStreamRequest, err
 var ConnectorService_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "border0.v1.ConnectorService",
 	HandlerType: (*ConnectorServiceServer)(nil),
-	Methods: []grpc.MethodDesc{
-		{
-			MethodName: "Register",
-			Handler:    _ConnectorService_Register_Handler,
-		},
-	},
+	Methods:     []grpc.MethodDesc{},
 	Streams: []grpc.StreamDesc{
-		{
-			StreamName:    "WatchMessages",
-			Handler:       _ConnectorService_WatchMessages_Handler,
-			ServerStreams: true,
-		},
 		{
 			StreamName:    "ControlStream",
 			Handler:       _ConnectorService_ControlStream_Handler,
